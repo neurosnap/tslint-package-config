5a613736db0dfdcf6ada17c8ccac0dc2e46e32f4
Eric Bower <neurosnap@gmail.com>
2018-05-13 09:50:43 -0400

    module boundary rule

--- rules/moduleBoundaryRule.ts
+++ rules/moduleBoundaryRule.ts
@@ -1,12 +1,17 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
+import { findImports, ImportKind } from 'tsutils';
 
 export class Rule extends Lint.Rules.AbstractRule {
   public static metadata: Lint.IRuleMetadata = {
     type: "functionality",
     ruleName: "module-boundary",
-    description: "Create strict module boundaries for packages",
+    description: "Disallows importing any submodule.",
     optionsDescription: Lint.Utils.dedent`
+      Submodules of some packages are treated as private APIs and the import
+      paths may change without deprecation periods. It's best to stick with
+      top-level package exports.
+
       Must provide namespace for packages.
     `,
     options: {
@@ -22,8 +27,7 @@ export class Rule extends Lint.Rules.AbstractRule {
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     const opts = this.getOptions();
-    const rule = new ModuleBoundaryWalker(sourceFile, opts)
-    return this.applyWithWalker(rule);
+    return this.applyWithFunction(sourceFile, walk, opts);
   }
 }
 
@@ -31,55 +35,29 @@ function createImportRe(namespace: string) {
   return new RegExp(`${namespace}\/(.+)\/`);
 }
 
-class ModuleBoundaryWalker extends Lint.RuleWalker {
-  public getNamespace() {
-    const opts = this.getOptions();
-    const namespace = opts[0];
-    return namespace;
-  }
+function getNamespace(opts: Lint.IOptions) {
+  return opts.ruleArguments[0];
+}
 
-  public visitVariableDeclaration(node: ts.VariableDeclaration) {
-    const namespace = this.getNamespace();
+function walk(ctx: Lint.WalkContext<Lint.IOptions>) {
+  for (const name of findImports(ctx.sourceFile, ImportKind.All)) {
+    const namespace = getNamespace(ctx.options);
     if (!namespace) {
       return;
     }
-    const importName = node.initializer.getText();
 
-    if (importName.indexOf('require') !== 0) {
-      return;
-    }
-
+    const importName = name.text;
     const importRe = createImportRe(namespace);
     const regResults = importRe.exec(importName);
 
     if (regResults) {
-      if (/types'\)$/.test(importName)) {
+      if (/types$/.test(importName)) {
         return;
       }
 
-      this.addFailureAtNode(
-        node,
-        `${Rule.FAILURE_STRING}, must import package like: ${namespace}/${regResults[1]}`,
-      )
-    }
-  }
-
-  public visitImportDeclaration(node: ts.ImportDeclaration) {
-    const namespace = this.getNamespace();
-    if (!namespace) {
-      return;
-    }
-    const importName = node.moduleSpecifier.getText();
-    const importRe = createImportRe(namespace);
-    const regResults = importRe.exec(importName);
-
-    if (regResults) {
-      if (/types'$/.test(importName)) {
-        return;
-      }
-
-      this.addFailureAtNode(
-        node,
+      ctx.addFailure(
+        name.getStart(ctx.sourceFile) + 1,
+        name.end - 1,
         `${Rule.FAILURE_STRING}, must import package like: ${namespace}/${regResults[1]}`,
       );
     }
